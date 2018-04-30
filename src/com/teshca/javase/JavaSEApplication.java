929572dea733a99ac8d38675524dda95bee8a190
Shifatullah Rizwan <5946849+Shifatullah@users.noreply.github.com>
2018-04-30 23:40:08 +1000

    added more collections show case

--- src/com/teshca/javase/JavaSEApplication.java
+++ src/com/teshca/javase/JavaSEApplication.java
@@ -5,13 +5,21 @@ import com.teshca.javase.collection.*;
 public class JavaSEApplication {
 
 	public static void main(String[] args) {
-		CollectionShowCase objCollectionShowCase = new CollectionShowCase();
 		
-		objCollectionShowCase.ArrayListShowCase();		
+		System.out.println("**************************************");
+		System.out.println("******** COLLECTIONS SHOWCASE ********");
+		System.out.println("**************************************");
+		System.out.println("\n");
 		
+		CollectionShowCase objCollectionShowCase = new CollectionShowCase();		
+		objCollectionShowCase.ArrayListShowCase();				
 		objCollectionShowCase.LinkedListShowCase();
-		
-		
+		objCollectionShowCase.HashSetShowCase();
+		objCollectionShowCase.LinkedHashSetShowCase();
+		objCollectionShowCase.TreeSetShowCase();
+		objCollectionShowCase.PriorityQueueShowCase();
+		objCollectionShowCase.ArrayDequeShowCase();
+		objCollectionShowCase.EnumSetShowCase();		
 		
 		System.out.println("Press any key to exit...");
 		System.console().readLine();
--- src/com/teshca/javase/collection/CollectionShowCase.java
+++ src/com/teshca/javase/collection/CollectionShowCase.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class CollectionShowCase {
 
 	public void ArrayListShowCase() {
-		System.out.println("ArrayList ShowCase");		
+		System.out.println("******** ArrayList ShowCase ********");		
 		
 		// Create ArrayList of String
 		ArrayList<String> stringArrayList = new ArrayList<String>();		
@@ -33,7 +33,7 @@ public class CollectionShowCase {
 	}
 	
 	public void LinkedListShowCase() {
-		System.out.println("LinkedList ShowCase");
+		System.out.println("******** LinkedList ShowCase ********");
 		
 		// Create LinkedList
 		LinkedList<String> stringLinkedList = new LinkedList<String>();		
@@ -74,5 +74,108 @@ public class CollectionShowCase {
 		// Change 3rd object value
 		stringLinkedList.set(2, "Four");
 		System.out.println("Set 3rd object to 'Four' using set(2, \"Four\"): " + stringLinkedList.get(2));
+	}
+	
+	public void HashSetShowCase() {
+		System.out.println("******** HashSet ShowCase ********");
+		
+		// Create HashSet
+		HashSet<String> stringHashSet = new HashSet<String>();		
+		System.out.println("HashSet created: " + stringHashSet + " - Size: " + stringHashSet.size());
+
+		// Add four objects HashSet
+		stringHashSet.add("Two");
+		stringHashSet.add("Three");
+		stringHashSet.add("Five");
+		stringHashSet.add("Nine");
+		System.out.println("Added four objects in HashSet: " + stringHashSet + " - Size: " + stringHashSet.size());
+	}
+	
+	public void LinkedHashSetShowCase() {
+		System.out.println("******** LinkedHashSet ShowCase ********");
+		
+		// Create LinkedHashSet
+		LinkedHashSet<String> stringLinkedHashSet = new LinkedHashSet<String>();		
+		System.out.println("LinkedHashSet created: " + stringLinkedHashSet + " - Size: " + stringLinkedHashSet.size());
+
+		// Add four objects LinkedHashSet
+		stringLinkedHashSet.add("Two");
+		stringLinkedHashSet.add("Three");
+		stringLinkedHashSet.add("Five");
+		stringLinkedHashSet.add("Nine");
+		System.out.println("Added four objects in LinkedHashSet: " + stringLinkedHashSet + " - Size: " + stringLinkedHashSet.size());
+	}
+	
+	public void TreeSetShowCase() {
+		System.out.println("******** TreeSet ShowCase ********");
+		
+		// Create TreeSet
+		TreeSet<String> stringTreeSet = new TreeSet<String>();		
+		System.out.println("TreeSet created: " + stringTreeSet + " - Size: " + stringTreeSet.size());
+
+		// Add four objects TreeSet
+		stringTreeSet.add("Two");
+		stringTreeSet.add("Three");
+		stringTreeSet.add("Five");
+		stringTreeSet.add("Nine");
+		System.out.println("Added four objects in TreeSet: " + stringTreeSet + " - Size: " + stringTreeSet.size());
+	}
+	
+	public void PriorityQueueShowCase() {
+		System.out.println("******** PriorityQueue ShowCase ********");
+		
+		// Create PriorityQueue
+		PriorityQueue<String> stringPriorityQueue = new PriorityQueue<String>();		
+		System.out.println("PriorityQueue created: " + stringPriorityQueue + " - Size: " + stringPriorityQueue.size());
+
+		// Add four objects PriorityQueue
+		stringPriorityQueue.add("Two");
+		stringPriorityQueue.add("Three");
+		stringPriorityQueue.add("Five");
+		stringPriorityQueue.add("Nine");
+		System.out.println("Added four objects in PriorityQueue: " + stringPriorityQueue + " - Size: " + stringPriorityQueue.size());
+		
+		// Poll
+		String pollObject = stringPriorityQueue.poll();
+		System.out.println("After poll() call on PriorityQueue: " + stringPriorityQueue + " poll object is: " + pollObject + " - Size: " + stringPriorityQueue.size());
+		
+		// Offer
+		boolean offerResult = stringPriorityQueue.offer("Ten");
+		System.out.println("After offer(\"Ten\") call on PriorityQueue: " + stringPriorityQueue + " offer() returned: " + offerResult + " - Size: " + stringPriorityQueue.size());
+	}
+	
+	public void ArrayDequeShowCase() {
+		System.out.println("******** ArrayDeque ShowCase ********");
+		
+		// Create ArrayDeque
+		ArrayDeque<String> stringArrayDeque = new ArrayDeque<String>();		
+		System.out.println("ArrayDeque created: " + stringArrayDeque + " - Size: " + stringArrayDeque.size());
+
+		// Add four objects ArrayDeque
+		stringArrayDeque.add("Two");
+		stringArrayDeque.add("Three");
+		stringArrayDeque.add("Five");
+		stringArrayDeque.add("Nine");
+		System.out.println("Added four objects in ArrayDeque: " + stringArrayDeque + " - Size: " + stringArrayDeque.size());
+		
+		// Push
+		String pollObject = stringArrayDeque.poll();
+		System.out.println("After poll() call on ArrayDeque: " + stringArrayDeque + " poll object is: " + pollObject + " - Size: " + stringArrayDeque.size());
+		
+		// Poll
+		stringArrayDeque.push("Ten");
+		System.out.println("After push(\"Ten\") call on ArrayDeque: " + stringArrayDeque + " - Size: " + stringArrayDeque.size());
+	}
+	
+	enum days {  
+		  SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY  
+		}
+	
+	public void EnumSetShowCase() {
+		System.out.println("******** EnumSet ShowCase ********");
+		
+		// Create EnumSet
+		Set<days> enumSet = EnumSet.of(days.TUESDAY, days.THURSDAY); 		
+		System.out.println("EnumSet created: " + enumSet + " - Size: " + enumSet.size());			
 	}
 }
